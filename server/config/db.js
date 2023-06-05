bd9cf8ebac903219497ba066579fef99cdbeb7f4
Mayur <mayurshinde0106@gmail.com>
2023-06-05 21:02:52 +0530

    Complete

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-const MongoUrl = "mongodb+srv://blog-pages:X54D8pZFqTJnx6h4@cluster0.4dpwnxm.mongodb.net/?retryWrites=true&w=majority"
+const MongoUrl = "mongodb+srv://blog-pages:X54D8pZFqTJnx6h4@cluster0.4dpwnxm.mongodb.net/Blog-App?retryWrites=true&w=majority"
 
 
 const InitiateMongoServer = async ()=>{
